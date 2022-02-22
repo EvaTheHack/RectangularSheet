286569a0718025e36e93290ffa309d74c53aa8be
Eva <evathehack@gmail.com>
2022-02-22 22:17:35 +0200

    remove attributes from model

--- Models/Detail.cs
+++ Models/Detail.cs
@@ -4,13 +4,10 @@ namespace RectangularSheet.Models
 {
     public class Detail
     {
-        [DisplayName("Ширина")]
-        public int Width { get; set; }
+        public int Width { get; set; } 
 
-        [DisplayName("Высота")]
         public int Height { get; set; }
 
-        [DisplayName("Кол-во")]
         public int Count { get; set; }
     }
 }
