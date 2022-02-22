e29c6e87e2585ef5650a4c84800f7b8a6f7c394e
Eva <evathehack@gmail.com>
2022-02-22 21:08:19 +0200

    Add detail model for binding

---
+++ Models/Detail.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace RectangularSheet.Models
+{
+    public class Detail
+    {
+        [DisplayName("Ширина")]
+        public int Width { get; set; }
+
+        [DisplayName("Высота")]
+        public int Height { get; set; }
+
+        [DisplayName("Кол-во")]
+        public int Count { get; set; }
+    }
+}
