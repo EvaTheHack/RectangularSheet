aea258b65c743e237c0b5ee309baa4c06bf156e6
Eva <evathehack@gmail.com>
2022-02-22 22:17:23 +0200

    Add mapper for detail dto to detail

---
+++ Utils/Mapper.cs
@@ -0,0 +1,29 @@
+﻿using RectangularSheet.Models;
+using System;
+using System.Collections.Generic;
+
+namespace RectangularSheet.Utils
+{
+    public static class Mapper
+    {
+        public static List<Detail> Map(this List<DetailDto> source)
+        {
+            var result = new List<Detail>();
+            foreach (var d in source)
+            {
+                if(d.Height == null || d.Width == null || d.Count == null)
+                {
+                    continue;
+                }
+                result.Add(new Detail
+                {
+                    Width = Convert.ToInt32(d.Width),
+                    Height = Convert.ToInt32(d.Height),
+                    Count = Convert.ToInt32(d.Count),
+                }); 
+            }
+
+            return result;
+        }
+    }
+}
