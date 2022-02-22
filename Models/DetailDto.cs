31ab3b198ee8c0378ca3c6b70466122a71f7e577
Eva <evathehack@gmail.com>
2022-02-22 22:17:06 +0200

    Add detail dto for binding

---
+++ Models/DetailDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace RectangularSheet.Models
+{
+    public class DetailDto
+    {
+        [DisplayName("Ширина")]
+        public int? Width { get; set; }
+
+        [DisplayName("Высота")]
+        public int? Height { get; set; }
+
+        [DisplayName("Кол-во")]
+        public int? Count { get; set; }
+    }
+}
